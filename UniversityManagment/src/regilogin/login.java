f35e7e43176595a39e1e25e69921472982183574
Mohak2 <114548361+Mohak2@users.noreply.github.com>
2023-12-04 12:03:51 +0530

    modifyed login

--- UniversityManagment/src/regilogin/login.java
+++ UniversityManagment/src/regilogin/login.java
@@ -74,8 +74,8 @@ public class login {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter username: ");
-            String enteredUsername = scanner.nextLine();
+            System.out.print("Enter useremail: ");
+            String enteredUseremail = scanner.nextLine();
 
             System.out.print("Enter password: ");
             String enteredPassword = scanner.nextLine();
@@ -83,9 +83,9 @@ public class login {
 
            
                 // Check the database for the entered username and password
-                String query = "SELECT * FROM user WHERE username = ? AND passsword = ? ";
+                String query = "SELECT * FROM user WHERE useremail= ? AND passsword = ? ";
                 try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                    preparedStatement.setString(1, enteredUsername);
+                    preparedStatement.setString(1, enteredUseremail);
                     preparedStatement.setString(2, enteredPassword);
                    
 
@@ -96,13 +96,23 @@ public class login {
 
                         if (storedDesignation.equalsIgnoreCase(b)) {
                             System.out.println("Login successful!");
-                            a = enteredUsername;
+                            String query1 = "SELECT username FROM user WHERE useremail = ?";
+                            try (PreparedStatement preparedStatement1 = connection.prepareStatement(query1)) {
+                                preparedStatement1.setString(1, enteredUseremail);
+                                ResultSet resultSet1 = preparedStatement1.executeQuery();
+                                if (resultSet1.next()) {
+                                    a = resultSet1.getString("username");
+                                }
+                            }
+                        catch (SQLException e) {
+                            e.printStackTrace();
+                            }
                         } else {
                             System.out.println("Designation mismatch. Please enter the correct designation.");
                         
                         }
                     } else {
-                        System.out.println("Invalid username or password");
+                        System.out.println("Invalid useremail or password");
                     }
                 }
 
@@ -115,7 +125,7 @@ public class login {
         
 
     }
-    public static String stud (String b)
+     public static String stud (String b)
     { 
         String jdbcUrl = "jdbc:mysql://localhost:3306/university";
         String username = "root";
@@ -126,8 +136,8 @@ public class login {
             Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Enter username: ");
-            String enteredUsername = scanner.nextLine();
+            System.out.print("Enter useremail: ");
+            String enteredUseremail = scanner.nextLine();
 
             System.out.print("Enter password: ");
             String enteredPassword = scanner.nextLine();
@@ -135,9 +145,9 @@ public class login {
 
            
                 // Check the database for the entered username and password
-                String query = "SELECT * FROM user WHERE username = ? AND passsword = ? ";
+                String query = "SELECT * FROM user WHERE useremail= ? AND passsword = ? ";
                 try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                    preparedStatement.setString(1, enteredUsername);
+                    preparedStatement.setString(1, enteredUseremail);
                     preparedStatement.setString(2, enteredPassword);
                    
 
@@ -148,13 +158,23 @@ public class login {
 
                         if (storedDesignation.equalsIgnoreCase(b)) {
                             System.out.println("Login successful!");
-                            a = enteredUsername;
+                            String query1 = "SELECT username FROM user WHERE useremail = ?";
+                            try (PreparedStatement preparedStatement1 = connection.prepareStatement(query1)) {
+                                preparedStatement1.setString(1, enteredUseremail);
+                                ResultSet resultSet1 = preparedStatement1.executeQuery();
+                                if (resultSet1.next()) {
+                                    a = resultSet1.getString("username");
+                                }
+                            }
+                        catch (SQLException e) {
+                            e.printStackTrace();
+                            }
                         } else {
                             System.out.println("Designation mismatch. Please enter the correct designation.");
                         
                         }
                     } else {
-                        System.out.println("Invalid username or password");
+                        System.out.println("Invalid useremail or password");
                     }
                 }
 
